d16e15e1c3beb9fe456ee470743c9dbde8da1d1d
Ryan Poolos <ryanpoolos@Gmail.com>
2013-02-19 20:02:57 -0500

    Initial Commit

---
+++ In App Purchase Demo/MRPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MRPAppDelegate.h
+//  In App Purchase Demo
+//
+//  Created by Ryan Poolos on 2/19/13.
+//  Copyright (c) 2013 PopArcade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MRPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ In App Purchase Demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  In App Purchase Demo
+//
+//  Created by Ryan Poolos on 2/19/13.
+//  Copyright (c) 2013 PopArcade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MRPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MRPAppDelegate class]));
+    }
+}
